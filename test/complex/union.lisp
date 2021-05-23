e96d129ba7c39d4e0666625063f6cacbc6ecefad
Sahil Kang <sahilkang@google.com>
2021-05-23 05:14:45 -0700

    Add late type checks to union

--- test/complex/union.lisp
+++ test/complex/union.lisp
@@ -94,3 +94,78 @@
     (is (null (avro:object (make-instance schema :object nil))))
     (signals error
       (make-instance schema :object 3))))
+
+(test late-type-check
+  (setf (find-class 'late_union) nil
+        (find-class 'late_fixed) nil)
+
+  (defclass late_union ()
+    ()
+    (:metaclass avro:union)
+    (:schemas avro:string late_fixed))
+
+  (signals error
+    (avro:schemas (find-class 'late_union)))
+
+  (defclass late_fixed ()
+    ()
+    (:metaclass avro:fixed)
+    (:size 12))
+
+  (is (eq (find-class 'late_fixed)
+          (elt (avro:schemas (find-class 'late_union)) 1))))
+
+(test no-schemas
+  (signals error
+    (make-instance
+     'avro:union
+     :schemas #())))
+
+(test non-schema
+  (setf (find-class 'some_union) nil
+        (find-class 'some_class) nil)
+
+  (defclass some_class ()
+    ())
+
+  (defclass some_union ()
+    ()
+    (:metaclass avro:union)
+    (:schemas avro:string some_class))
+
+  (signals error
+    (avro:schemas (find-class 'some_union))))
+
+(test duplicate-primitive
+  (let ((schema (make-instance
+                 'avro:union
+                 :schemas '(avro:string avro:null avro:string))))
+    (signals error
+      (avro:schemas schema))))
+
+(test duplicate-array
+  (let* ((array<int> (make-instance 'avro:array :items 'avro:int))
+         (array<string> (make-instance 'avro:array :items 'avro:string))
+         (schema (make-instance
+                  'avro:union
+                  :schemas (list 'avro:null array<int> array<string>))))
+    (signals error
+      (avro:schemas schema))))
+
+(test duplicate-named
+  (let* ((fixed (make-instance 'avro:fixed :name "foo" :size 12))
+         (enum (make-instance 'avro:enum :name "foo" :symbols '("FOO" "BAR")))
+         (schema (make-instance
+                  'avro:union
+                  :schemas (list 'avro:null fixed enum))))
+    (signals error
+      (avro:schemas schema))))
+
+(test different-name-same-type
+  (let* ((fixed-1 (make-instance 'avro:fixed :name "foo" :size 12))
+         (fixed-2 (make-instance 'avro:fixed :name "bar" :size 12))
+         (schema (make-instance
+                  'avro:union
+                  :schemas (list 'avro:null fixed-1 fixed-2))))
+    (is (equalp (vector 'avro:null fixed-1 fixed-2)
+                (avro:schemas schema)))))
